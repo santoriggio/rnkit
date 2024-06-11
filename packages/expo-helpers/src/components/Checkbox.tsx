6113f3b2dce0c9c8fdca8fde2f7a69b9bfca702b
Santo <info@santoriggio.it>
2024-06-11 15:44:08 +0200

    feat: create Checkbox component
    ---

--- packages/expo-helpers/src/components/Checkbox.tsx
+++ packages/expo-helpers/src/components/Checkbox.tsx
@@ -1,16 +1,36 @@
 import { View } from "react-native";
 import { useStyles } from "../hooks";
-import { useMemo } from "react";
+import { ReactNode, useMemo } from "react";
 import { Entypo } from "@expo/vector-icons";
+import Box from "./Box";
+import Text from "./Text";
 type CheckboxProps = {
   /*
    * @default primary
    * */
   color?: string;
   selected: boolean;
+  title?: string;
+  placeholder?: string | ((props: CheckboxProps) => ReactNode);
+  required?: boolean;
 };
+function Placeholder(props: CheckboxProps) {
+  if (typeof props.placeholder === "undefined") return null;
+
+  if (typeof props.placeholder === "string") {
+    return (
+      <Text numberOfLines={1} marginLeft="s">
+        {props.placeholder}
+      </Text>
+    );
+  }
+  if (typeof props.placeholder === "function") {
+    return props.placeholder(props);
+  }
+  return null;
+}
 export default function Checkbox(props: CheckboxProps) {
-  const { selected, color } = props;
+  const { selected, color, required = false } = props;
   const { colors, spacing } = useStyles();
 
   const tint = useMemo(() => {
@@ -19,20 +39,39 @@ export default function Checkbox(props: CheckboxProps) {
     }
 
     return colors.primary;
-  }, [color, colors.primary, selected]);
-
+  }, [color, colors.primary]);
   return (
-    <View
-      style={{
-        borderWidth: 2.2,
-        backgroundColor: selected ? tint : undefined,
-        borderColor: selected ? tint : colors.gray,
-        borderRadius: spacing.s,
-        height: spacing.l,
-        width: spacing.l,
-      }}
-    >
-      {selected && <Entypo name="check" color={"white"} size={12} />}
+    <View>
+      {props.title && props.title !== "" && (
+        <Box horizontal marginBottom="s">
+          <Text bold numberOfLines={1} size="l">
+            {props.title}{" "}
+          </Text>
+          {required && (
+            <Text size="l" bold color={colors.danger}>
+              *
+            </Text>
+          )}
+        </Box>
+      )}
+      <Box horizontal>
+        <Box
+          marginRight="s"
+          style={{
+            borderWidth: 2.2,
+            backgroundColor: selected ? tint : undefined,
+            borderColor: selected ? tint : colors.gray,
+            borderRadius: spacing.s,
+            height: spacing.l,
+            width: spacing.l,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {selected && <Entypo name="check" color={"white"} size={spacing.m} />}
+        </Box>
+        <Placeholder {...props} />
+      </Box>
     </View>
   );
 }
