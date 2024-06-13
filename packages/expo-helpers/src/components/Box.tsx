05f25283de08ff47979249e05a4cc2809b2a9b12
Santo <info@santoriggio.it>
2024-06-13 13:45:36 +0200

    fix: make box a little faster
    use react.createElement and use TouchableOpacity only if onPress or
    onLongPress exists

--- packages/expo-helpers/src/components/Box.tsx
+++ packages/expo-helpers/src/components/Box.tsx
@@ -3,9 +3,11 @@ import {
   ColorValue,
   TouchableOpacity,
   TouchableOpacityProps,
+  View,
+  ViewProps,
 } from "react-native";
 import useStyles, { SpacingSizeKey } from "../hooks/useStyles";
-import { PropsWithChildren, useMemo } from "react";
+import React, { PropsWithChildren, useMemo } from "react";
 import { useRawSpacingProps } from "../hooks/useSpacingProps";
 
 type BoxProps = {
@@ -34,33 +36,46 @@ export type SpacingProps = Partial<
   Record<keyof typeof spacingProps, SpacingSizeKey>
 >;
 
-export default function Box(props: PropsWithChildren<BoxProps>) {
-  const { backgroundColor, horizontal } = props;
+function Box({
+  backgroundColor,
+  horizontal,
+  onPress,
+  onLongPress,
+  ...props
+}: PropsWithChildren<BoxProps>) {
   const { colors } = useStyles();
   const spacingProps = useRawSpacingProps(props);
-  const disabled = !(
-    typeof props.onPress === "function" ||
-    typeof props.onLongPress === "function"
-  );
+  // const disabled = !(
+  //   typeof props.onPress === "function" ||
+  //   typeof props.onLongPress === "function"
+  // );
   const styles = useMemo(() => {
-    const bg = backgroundColor || undefined;
     return StyleSheet.create({
       container: {
         flexDirection: horizontal ? "row" : "column",
-        backgroundColor: bg,
+        backgroundColor: backgroundColor || undefined,
         borderColor: colors.border,
         ...spacingProps,
       },
     });
   }, [backgroundColor, spacingProps, horizontal, colors.border]);
-  return (
-    <TouchableOpacity
-      disabled={disabled}
-      activeOpacity={0.5}
-      {...props}
-      style={[styles.container, props.style]}
-    >
-      {props.children}
-    </TouchableOpacity>
-  );
+
+  const componentProps: ViewProps = {
+    ...props,
+    style: [styles.container, props.style],
+    children: props.children,
+  };
+
+  if (typeof onPress === "function" || typeof onLongPress === "function") {
+    return React.createElement(TouchableOpacity, {
+      ...componentProps,
+      activeOpacity: 0.8,
+      onPress,
+      onLongPress,
+    });
+  }
+
+  return React.createElement(View, componentProps);
 }
+
+export default Box;
